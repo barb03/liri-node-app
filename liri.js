81815416c00383ab19498f63bd9eae79056ad2d9
Vader55 <38086201+barb03@users.noreply.github.com>
2018-11-19 18:33:04 -0600

    added comments to js file

--- liri.js
+++ liri.js
@@ -3,8 +3,10 @@ require("dotenv").config();
 
 //Add the code required to import the `keys.js` file and store it in a variable.
 var keys = require('./keys.js');
+var fs = require("fs");
+
+var action = process.argv[2];
+var value = process.argv[3];
 
 //You should then be able to access your keys information like so
 var spotify = new Spotify(keys.spotify);
-
-//I want to see if gitignore is ignoring the dot.env file.
