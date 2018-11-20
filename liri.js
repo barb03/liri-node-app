226c08d93d6fffd9048ce3c51b068589d320b0d4
Vader55 <38086201+barb03@users.noreply.github.com>
2018-11-19 20:30:01 -0600

    added base omdb request

--- liri.js
+++ liri.js
@@ -1,12 +1,57 @@
-//At the top of the `liri.js` file, add code to read and set any environment variables with the dotenv package:
-require("dotenv").config();
 
-//Add the code required to import the `keys.js` file and store it in a variable.
-var keys = require('./keys.js');
-var fs = require("fs");
+// var spotify = require('spotify');
+var request = require('request');
+var fs = require('fs');
+var keys = require("./keys.js");
 
-var action = process.argv[2];
-var value = process.argv[3];
+var inputCommand = process.argv[2];
+var commandParam = process.argv[3];
+var defaultMovie = "Mr. Nobody";
+var axios = require('axios');
 
-//You should then be able to access your keys information like so
-var spotify = new Spotify(keys.spotify);
+
+function processCommands(command, commandParam){
+
+	console.log(commandParam);
+
+	switch(command){	
+	
+	case 'movie-this':		
+		if(commandParam === undefined){
+			commandParam = defaultMovie;
+		}    
+		movieThis(commandParam); break;
+	case 'do-what-it-says':
+		doWhatItSays(); break;
+	default: 
+		console.log("That is not a valid command");
+    }
+}
+
+function movieThis(movieName){    
+	console.log(movieName);
+	axios.get("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy").then(
+  function(response) {
+    console.log("The movie's rating is: " + response.data.imdbRating);
+  });
+
+}
+
+function doWhatItSays(){
+	fs.readFile('random.txt', 'utf8', function(err, data){
+
+		if (err){ 
+			return console.log(err);
+		}
+
+		var dataArr = data.split(',');
+
+		processCommands(dataArr[0], dataArr[1]);
+	});
+}
+
+
+
+//-------------------------MAIN PROCESS-------------------------------------------
+
+processCommands(inputCommand, commandParam);
