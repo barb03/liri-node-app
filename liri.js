2d8de1e50d62206f57f7c6230eb71f4acb864675
Vader55 <38086201+barb03@users.noreply.github.com>
2018-11-20 20:21:21 -0600

    added spotify

--- liri.js
+++ liri.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
-var keys = require("./keys.js");
+// var keys = require("./keys.js");
 
 // var Twitter = required('twitter');
-var spotify = require('spotify');
-// var spotify = new Spotify(keys.spotify);
+
+var Spotify = require('node-spotify-api');
+var keys = require('./keys.js');
+var spotify = new Spotify(keys.spotify);
 var request = require('request');
 var fs = require('fs');
 
@@ -21,22 +23,21 @@ var fs = require('fs');
 // 	});
 // }
 
-// var getArtistNames = function(artist){
-// 	return artist.name;
-// }
+var getArtistNames = function(artist){
+	return artist.name;
+}
 
  var getMySpotify = function(songName){
 	spotify.search({ type: 'track', query: songName }, function(err, data) {
 		if ( err ) {
 			console.log('Error occurred: ' + err);
 			return;
-		}
-	 
-	   console.log(data);
+		}	
+
 	   var songs = data.tracks.items;
 	   for(var i=0; i<songs.length; i++){
 		   console.log(i);
-		   console.log('artist(s): ' + songs[i].artists.map(getArtistNames));
+		   console.log('artist(s): ' + songs[i].artists.map(getArtistNames));		   
 		   console.log('song name: ' + songs[i].name);
 		   console.log('preview song: ' + songs[i].preview_url);
 		   console.log('album: ' + songs[i].album.name);
